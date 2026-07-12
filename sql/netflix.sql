dc0e55b660f9d0e7a6ded328be5a52456a8df65b
Shubham-salvi1 <shubham.salvi@ucdconnect.ie>
2026-07-12 01:56:58 +0100

    Add initial project files: dataset, SQL scripts, Power BI dashboard, README, and schema diagram

---
+++ sql/netflix.sql
@@ -0,0 +1,98 @@
+CREATE DATABASE netflix_challenge ;
+USE netflix_challenge ;
+
+CREATE TABLE users (
+user_id INT PRIMARY KEY,
+signup_date DATE,
+country VARCHAR(150),
+plan_type VARCHAR(100)
+);
+
+CREATE TABLE content(
+content_id INT PRIMARY KEY,
+title VARCHAR(100),
+genre VARCHAR(50),
+content_type VARCHAR(100),
+release_year INT
+);
+
+CREATE TABLE watch_history (
+watch_id INT PRIMARY KEY,
+user_id INT,
+content_id INT,
+watch_date DATE,
+minutes_watched INT,
+FOREIGN KEY (user_id) REFERENCES users(user_id),
+FOREIGN KEY(content_id) REFERENCES content(content_id)
+);
+-- task 1: Show first 10 rows from users table
+SELECT * FROM  users
+LIMIT 10;
+
+-- task 2: Count total number of users
+
+SELECT COUNT(user_id)  FROM users;
+
+-- task 3: 
+ SELECT COUNT(*) FROM watch_history;
+ 
+ -- task 4: filtering, count users from Ireland
+ SELECT * FROM users
+ WHERE country = 'Ireland';
+ 
+ SELECT * FROM users
+ WHERE plan_type = 'Premium'; 
+ 
+SELECT * FROM users
+WHERE plan_type = 'Premium' AND country = 'Ireland';
+
+SELECT * FROM users 
+WHERE country = 'Ireland' OR 'Germany';
+
+-- task: Count total minutes watched across all users
+SELECT SUM(minutes_watched) FROM watch_history;
+
+-- SELECT genre, SUM(minutes_watched)
+-- FROM watch_history
+-- JOIN content
+-- ON  watch_history.content_id = content.content_id
+-- GROUP BY genre;
+-- task : Find total minutes watched BY genre
+-- SELECT c.genre, SUM(w.minutes_watched) AS total_minutes
+-- FROM watch_history w
+-- JOIN content c 
+-- ON w.content_id = c.content_id
+-- GROUP BY c.genre;
+
+-- task: Find the top 10 most watched titles by total minutes watched
+SELECT c.title, SUM(w.minutes_watched) AS total_minutes
+FROM watch_history w
+JOIN content c
+ON w.content_id = c.content_id
+GROUP BY c.title
+ORDER BY total_minutes DESC
+LIMIT 10;
+
+-- task : total mintues watched per user
+SELECT user_id, SUM(minutes_watched) AS total_minutes
+FROM watch_history
+GROUP BY user_id
+ORDER BY total_minutes DESC
+LIMIT 10;
+
+-- task : avg watch time by plan_type
+SELECT u.plan_type, AVG(w.minutes_watched) AS avg_time
+FROM watch_history w
+JOIN users u 
+ON w.user_id = u.user_id
+GROUP BY u.plan_type;
+
+-- task: Find TOTAL watch time AND AVERAGE watch time by plan_type
+SELECT u.plan_type, 
+AVG(w.minutes_watched) AS avg_time,
+SUM(w.minutes_watched) AS total_minutes
+FROM watch_history w
+JOIN users u 
+ON w.user_id = u.user_id
+GROUP BY u.plan_type;
+
